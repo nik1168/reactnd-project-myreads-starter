c5809332fac0b387d21f8174c4f9a0654d7e4642
nik1168 <ngeisser32@gmail.com>
2017-08-23 22:01:23 -0400

    Update Shelf.js

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,1 +1,24 @@
+import React, { Component } from 'react';
+import Book from './Books'
 
+function Shelf(props) {
+  return(
+    <div>
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {
+              props.books.map((book,index)=>(
+                <li key={index}>
+                  <Book book={book} onChangeShelf={props.onChangeShelf}/>
+                </li>
+              ))
+            }
+          </ol>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Shelf
